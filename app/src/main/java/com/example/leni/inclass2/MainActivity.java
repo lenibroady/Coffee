3330abeea8b4755d62c895582580a0ecf8556ff3
Leni Broady <leni.broady@gmail.com>
2018-10-11 10:49:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/leni/inclass2/MainActivity.java
@@ -0,0 +1,57 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+package com.example.leni.inclass2;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+int total=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        displayprice();
+    }
+public void increment(View view){
+        total++;
+        display();
+
+}
+    public void decrement(View view){
+        total--;
+        display();
+
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display() {
+        TextView quantityTextView = (TextView) findViewById(R.id.number);
+        quantityTextView.setText("" + total);
+    }
+    private void displayprice() {
+        TextView quantityTextView = (TextView) findViewById(R.id.dollar);
+        quantityTextView.setText("$" + total*5);
+    }
+}
